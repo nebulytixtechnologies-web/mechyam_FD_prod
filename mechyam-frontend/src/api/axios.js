8829c98e0fa768c30ab7321c26b299bda7773b29
nebulytixtechnologies-web <236551710+nebulytixtechnologies-web@users.noreply.github.com>
2025-12-29 15:00:02 +0530

    Update axios.js

--- mechyam-frontend/src/api/axios.js
+++ mechyam-frontend/src/api/axios.js
@@ -1,7 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+// });
+
+// export default api;
+
+// src/api/axios.js
+import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "/api", // nginx handles this
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // optional (cookies/JWT)
 });
 
 export default api;
+
