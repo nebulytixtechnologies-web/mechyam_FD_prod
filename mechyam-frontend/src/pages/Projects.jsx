c667f598e2579d807d45216c20a6a5b132452f3f
nebulytixtechnologies-web <236551710+nebulytixtechnologies-web@users.noreply.github.com>
2026-01-03 00:39:10 +0530

    Update Projects.jsx

--- mechyam-frontend/src/pages/Projects.jsx
+++ mechyam-frontend/src/pages/Projects.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-import api from "../../api/axios";
+import api from "../../api/axios.js";
 
 
 const Projects = () => {
